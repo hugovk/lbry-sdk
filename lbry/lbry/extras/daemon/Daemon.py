e17b2b61e62345e15e83e6fadcb0a6df0b12725b
Lex Berezhny <lex@damoti.com>
2019-10-02 09:32:27 -0400

    bug fixes related to cross-account / wallet operations

--- lbry/lbry/extras/daemon/Daemon.py
+++ lbry/lbry/extras/daemon/Daemon.py
@@ -1514,7 +1514,12 @@ class Daemon(metaclass=JSONRPCServerType):
         Returns: {Transaction}
         """
         wallet = self.wallet_manager.get_wallet_or_default(wallet_id)
-        account = wallet.get_account_or_default(account_id)
+        if account_id:
+            account = wallet.get_account_or_error(account_id)
+            accounts = [account]
+        else:
+            account = wallet.default_account
+            accounts = wallet.accounts
 
         amount = self.get_dewies_or_error("amount", amount)
         if not amount:
@@ -1535,14 +1540,14 @@ class Daemon(metaclass=JSONRPCServerType):
             )
 
         tx = await Transaction.create(
-            [], outputs, [account], account
+            [], outputs, accounts, account
         )
 
         if not preview:
             await self.ledger.broadcast(tx)
             await self.analytics_manager.send_credits_sent()
         else:
-            await account.ledger.release_tx(tx)
+            await self.ledger.release_tx(tx)
 
         return tx
 
